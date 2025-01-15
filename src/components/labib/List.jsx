de4e881e27390fcdddc5dee2b86a553d271b8593
MFDR-2401 <mfdr2401@gmail.com>
2025-01-15 18:37:43 +0600

    add a labib folder

---
+++ src/components/labib/List.jsx
@@ -0,0 +1,11 @@
+const List = ()=> {
+    return (
+        <div className='py-4'>
+            <ul>
+                <li className='d-flex gap-4 align-items-center'> <span>Text</span> <button className='btn btn-primary'>Edit</button> <button className='btn btn-danger'>Delete</button></li>
+            </ul>
+        </div>
+    )
+}
+
+export default List
